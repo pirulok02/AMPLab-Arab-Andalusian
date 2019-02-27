e5cae551ffc186287e34568b480862948a7b0e82
Victor Badenas Crespo <victor.badenas@gmail.com>
2019-02-27 19:24:22 +0100

    deleted papers folder

---
+++ music21setup.py
@@ -0,0 +1,23 @@
+from music21 import *
+
+def printsettings(us):
+    for key in sorted(us.keys()):
+        try:
+            tmp = us[key]
+        except:
+            tmp = "None"
+        print("{0} - {1}".format(key,tmp))
+
+
+us = environment.UserSettings()
+#us.create()
+
+printsettings(us)
+
+configure.run()
+
+printsettings(us)
+#environment.set('musicxmlPath','/usr/bin/mscore')
+#environment.set('showFormat','lilypond')
+#environment.set('musescoreDirectPNGPath','/usr/bin/mscore')
+#environment.set('lilypondPath','/usr/bin/lilypond')
